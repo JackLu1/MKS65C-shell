07c92a3106ad03389d9290b406e58a7e99242edc
Jack Lu <jlu9@stuy.edu>
2018-11-30 15:30:54 -0500

    ;

--- cshell.c
+++ cshell.c
@@ -6,18 +6,31 @@
 #include <errno.h>
 #include "shell.h"
 
+/*
+ * Runs infinite while loop until terminated
+ */
 int main(){
 
+    char ** cmd_list;
     char ** cmd;
     char * cwd = malloc(100);
     char * usr = getlogin();
+    char * delim = malloc(2);
     while(1){
         cwd = getcwd(cwd, 100);
         printf("[%s] [%s] $ ", usr, cwd);
-
-        cmd = parse();
-        shell_exe(cmd);
-        exe( cmd );
+        char * buffer = calloc(1,1000);
+        fgets(buffer, 1000, stdin);
+    
+        strcpy(delim, ";");
+        cmd_list = parse(buffer, delim);
+        strcpy(delim, " ");
+        int i = 0;
+        while( cmd_list[i] ) {
+            cmd = parse(cmd_list[i++], " ");
+            shell_exe(cmd);
+            exe( cmd );
+        }
     }
     return 0;
 }
@@ -26,17 +39,15 @@ int main(){
  * Responsible for receiving user input and parsing into an execvp array.
  * Returns array of strings of command line arguments.
  */
-char ** parse(){
+char ** parse( char * buffer, char * delim ){
 
-    char * buffer = calloc(1,1000);
     char ** args = calloc(1, 1000);
-    fgets(buffer, 1000, stdin);
 
     //remove terminating newline
     buffer[strcspn(buffer, "\n")] = 0;
 
     int i = 0;
-    while (args[i++] = strsep(&buffer, " "));
+    while (args[i++] = strsep(&buffer, delim));
     free(buffer);
     return args;
 }
@@ -71,8 +82,5 @@ void exe( char ** cmd ){
         exit(status);
     }
 }
-
-
-
 
 
--- shell.h
+++ shell.h
@@ -1,6 +1,19 @@
-// parse into execvp format
-char ** parse();
-// checks and handles shell commands
+/*
+ * Responsible for receiving user input and parsing into an execvp array.
+ * Returns array of strings of command line arguments.
+ */
+char ** parse(char * buffer, char * delim);
+/*
+ * Parses input for commands separated by ;
+ */
+char ** parse_cmd(char * buffer);
+/*
+ * Arguments: array of commandline argument strings
+ * Checks if command is special shell commands, and executes it if true.
+ */
 void shell_exe( char ** cmd );
-// executes non-shell commands
+/*
+ * Arguments: array of commandline argument strings
+ * Forks a child which executes command
+ */
 void exe( char ** cmd );
